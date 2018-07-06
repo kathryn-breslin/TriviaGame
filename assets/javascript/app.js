a5ba85bc533f28937d47118220a04c65fa96884a
Kathryn Breslin <kathryn.l.breslin@gmail.com>
2018-07-06 14:37:57 -0500

    Working on Results Page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,31 @@
-//create variables
-//set the timer to countdown after start button is pressed
-//create radio buttons that only capture one response
-//'Done' button that will submit answers
-//Page 'clears' are reveals Number of Correct Answers, Incorrect Answers, and those left unanswered
-
-//create radion buttons
-
-// function quizQuestion(question, answerChoices, correctAnswer) {
-//     this.question = question;
-//     this.answerChoices = answerChoices;
-//     this.correctAnswer = correctAnswer;
-// }
-
 var questions = [{
     question: "What year did the Tour de France begin?",
     answerChoices: ["1925", "1903", "1848", "2001"], 
-    correctAnwer: 2
+    correctAnwer: [1]
 },
 
 {
     question: "Who was the first winner of the Tour?",
     answerChoices: ["Henri Cornet", "Lance Armstrong", "Michael Phelps", "Maurice Garin"],
-    correctAnswer: 4
+    correctAnswer: [3]
 },
 
 {
     question: "What mountain ranges does the race pass through?",
     answerChoices: ["Alps", "Himalayas", "Pyrenees", "Rockies"],
-    correctAnswer: 1
+    correctAnswer: [0]
 },
 
 {
     question: "How many miles a day do the cyclists have to ride?",
     answerChoices: ["500", "400", "100", "200"],
-    correctAnswer: 3
+    correctAnswer: [2]
 },
 
 {
     question: "What is the total distance the racers will have traveled by the end of the Tour?",
     answerChoices: ["2,200", "9,050", "800", "1,370"],
-    correctAnswer: 1
+    correctAnswer: [0]
 }
 ];
 
@@ -47,15 +33,15 @@ var currentQuestion = 0;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 
-
 function questionOptions() {
 
     $('#container').hide();
     $('#next').hide();
     $('#timeRemaining').hide();
+    $('#submit').hide();
+    $('#results').hide();
 
     $('#question').html(parseInt(currentQuestion) + 1 + ". " + questions[currentQuestion].question);
-    //var options = questions[currentQuestion].answerChoices;
     var formHtml = '';
         
     for (var i = 0; i < questions.length; i++) {
@@ -64,27 +50,32 @@ function questionOptions() {
 
     $('#form').html(formHtml);
     $('#option').prop('checked', true);
-    
 
 };
 
+//not functioning
 function checkAnswers() {
-    if($('input[name=option]:checked').val() === questions[currentQuestion].correctAnswer) {
+    if($('input[type=radio]:checked').val() === questions[currentQuestion].correctAnswer) {
         correctAnswers++;
+        console.log(this.val);
     }
-    else if ($('input[name=option]:checked').val() != questions[currentQuestion].correctAnswer) {
+    else if ($('input[type=radio]:checked').val() != questions[currentQuestion].correctAnswer) {
         incorrectAnswers++;
+        console.log(this.val);
     }
+    
 };
 
 $(document).ready(function() {
 var start = $('#start');
 var next = $('#next');
-var results = $('#results');
+var results = $('#results'); //not functioning
 var stop;
 
 start.click(function() {
     $("#start").hide();
+    $('#submit').hide();
+    $('#results').hide();
     $('#timeRemaining').show();
     $('#container').show();
     $('#next').show();
@@ -99,33 +90,36 @@ start.click(function() {
     if (time === 0) {
         stop();
         clearInterval(time);
+        $('#results').show();
         }
     }, 1000);
     });
 
 next.click(function() {
     event.preventDefault();
-    checkAnswers();
+    checkAnswers(); //incorrect
     currentQuestion++;
     questionOptions();
     $('#container').show();
     $('#next').show();
     $('#timeRemaining').show();
-    
+    $('#submit').hide();
+    $('#results').hide();
 });
 
+//not functioning
 if (currentQuestion < questions.length) {
     questionOptions();
-    if (currentQuestion === questions.length -1) {
-        $('#next').html("Submit");
-        $('#next').click(function() {
+}
+    else if (currentQuestion === questions.length -1) {
+        $('#next').append('#submit');
+       // $('#submit').show();
+        $('#submit').click(function() {
             $('#results').show();
             $('#results').html("Correct Answers: " + correctAnswers + "Incorrect Answers: " + incorrectAnswers);
-        
-           // $('#results').html("Incorrect Answers: " + incorrectAnswers);
-            });
+          });
+          
         };
-    };
  
 });
 
