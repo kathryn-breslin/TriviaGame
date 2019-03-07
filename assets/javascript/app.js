4da8bf93d41080b26e9dcf87f98544c00ab2e89d
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-03-07 10:47:07 -0500

    js overhaul

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,126 +1,108 @@
 var questions = [{
-    question: "What year did the Tour de France begin?",
-    answerChoices: ["1925", "1903", "1848", "2001"], 
-    correctAnwer: [1]
+    question: "In what year did the Tour de France begin?",
+    answerChoices: ["1925", "1903", "1848", "2001"],
+    correctAnswer: "1"
 },
 
 {
     question: "Who was the first winner of the Tour?",
     answerChoices: ["Henri Cornet", "Lance Armstrong", "Michael Phelps", "Maurice Garin"],
-    correctAnswer: [3]
+    correctAnswer: "3"
 },
 
 {
-    question: "What mountain ranges does the race pass through?",
+    question: "Through which mountain range does the race pass?",
     answerChoices: ["Alps", "Himalayas", "Pyrenees", "Rockies"],
-    correctAnswer: [0]
+    correctAnswer: "0"
 },
 
 {
-    question: "How many miles a day do the cyclists have to ride?",
+    question: "The cyclists must race how many miles per day?",
     answerChoices: ["500", "400", "100", "200"],
-    correctAnswer: [2]
+    correctAnswer: "2"
 },
 
 {
     question: "What is the total distance the racers will have traveled by the end of the Tour?",
     answerChoices: ["2,200", "9,050", "800", "1,370"],
-    correctAnswer: [0]
+    correctAnswer: "0"
 }
 ];
 
-var currentQuestion = 0;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
+var currentQuestion = 0;
+var correct;
+var question;
+var sum = correctAnswers + incorrectAnswers;
+var container = $('#container');
 
-function questionOptions() {
-
-    $('#container').hide();
-    $('#next').hide();
-    $('#timeRemaining').hide();
-    $('#submit').hide();
-    $('#results').hide();
-
-    $('#question').html(parseInt(currentQuestion) + 1 + ". " + questions[currentQuestion].question);
-    var formHtml = '';
-        
-    for (var i = 0; i < questions.length; i++) {
-        formHtml += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"><label for="option' + i + '">' + questions[currentQuestion].answerChoices[i] + '</label></div><br/>';
-    }
-
-    $('#form').html(formHtml);
-    $('#option').prop('checked', true);
-
-};
-
-//not functioning
-function checkAnswers() {
-    if($('input[type=radio]:checked').val() === questions[currentQuestion].correctAnswer) {
-        correctAnswers++;
-        console.log(this.val);
-    }
-    else if ($('input[type=radio]:checked').val() != questions[currentQuestion].correctAnswer) {
-        incorrectAnswers++;
-        console.log(this.val);
-    }
-    
-};
-
-$(document).ready(function() {
-var start = $('#start');
-var next = $('#next');
-var results = $('#results'); //not functioning
-var stop;
-
-start.click(function() {
-    $("#start").hide();
-    $('#submit').hide();
-    $('#results').hide();
-    $('#timeRemaining').show();
-    $('#container').show();
-    $('#next').show();
+$(document).ready(function () {
+    container.hide();
+    var start = $('#start');
     var time = 30;
-    setInterval(function() {
-    time--; 
-    
-    if (time >= 0) {
-    $('#timeRemaining').html("<h4>" + time + " Seconds <h4>");
 
-    }
-    if (time === 0) {
-        stop();
-        clearInterval(time);
-        $('#results').show();
-        }
-    }, 1000);
+    //Starting the game and the clock
+    start.click(function () {
+        displayQuestion();
+        setInterval(function () {
+            time--;
+            container.show();
+            start.hide();
+            if (time >= 0) {
+                $('#timeRemaining').html("<h4>" + time + " Seconds <h4>");
+            }
+            if (time === 0) {
+                stop();
+                clearInterval(time);
+                showResults();
+            }
+        }, 1000);
     });
 
-next.click(function() {
-    event.preventDefault();
-    checkAnswers(); //incorrect
-    currentQuestion++;
-    questionOptions();
-    $('#container').show();
-    $('#next').show();
-    $('#timeRemaining').show();
-    $('#submit').hide();
-    $('#results').hide();
-});
+    //Display questions in the DOM
+    function displayQuestion() {
+        question = questions[currentQuestion].question;
+        correct = questions[currentQuestion].correctAnswer;
+        $('#question').html(question);
 
-//not functioning
-if (currentQuestion < questions.length) {
-    questionOptions();
-}
-    else if (currentQuestion === questions.length -1) {
-        $('#next').append('#submit');
-       // $('#submit').show();
-        $('#submit').click(function() {
-            $('#results').show();
-            $('#results').html("Correct Answers: " + correctAnswers + "Incorrect Answers: " + incorrectAnswers);
-          });
-          
-        };
- 
+        for (var i = 0; i < 4; i++) {
+            var answers = questions[currentQuestion].answerChoices[i];
+            $('#form').append('<div><button type="button" class="btn btn-lg btn-block btn-outline-secondary" name="option" value="' + i + '" id="' + i + '">' + answers + '</button></div>');
+            $('#option').prop('checked', true);
+        }
+        $('.btn-lg').on("click", function () {
+            var id = $(this).attr('id');
+            if (id === correct) {
+                correctAnswers++;
+                $('#form').empty();
+                currentQuestion++;
+                console.log("Correct!")
+                showResults();
+
+            }
+            else if (id !== correct) {
+                console.log("WRONG!");
+                incorrectAnswers++;
+                currentQuestion++;
+                $('#form').empty();
+                showResults();
+            }
+
+        })
+    }
+    // displayQuestion();
+    //Function to show Results
+    function showResults() {
+        if (currentQuestion === 5) {
+            $('#correct').html("Correct:" + correctAnswers);
+            $('#incorrect').html("Incorrect:" + incorrectAnswers);
+            $('#container').empty();
+        }
+        else {
+            displayQuestion();
+        }
+    }
 });
 
 
