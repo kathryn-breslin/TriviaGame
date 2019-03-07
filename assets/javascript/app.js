3c2cdce5a2c1a3551683bb4f580baa2649396f16
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-03-07 16:52:45 -0500

    css and js changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,77 @@
-var questions = [{
-    question: "In what year did the Tour de France begin?",
-    answerChoices: ["1925", "1903", "1848", "2001"],
-    correctAnswer: "1"
-},
-
-{
-    question: "Who was the first winner of the Tour?",
-    answerChoices: ["Henri Cornet", "Lance Armstrong", "Michael Phelps", "Maurice Garin"],
-    correctAnswer: "3"
-},
-
-{
-    question: "Through which mountain range does the race pass?",
-    answerChoices: ["Alps", "Himalayas", "Pyrenees", "Rockies"],
-    correctAnswer: "0"
-},
-
-{
-    question: "The cyclists must race how many miles per day?",
-    answerChoices: ["500", "400", "100", "200"],
-    correctAnswer: "2"
-},
-
-{
-    question: "What is the total distance the racers will have traveled by the end of the Tour?",
-    answerChoices: ["2,200", "9,050", "800", "1,370"],
-    correctAnswer: "0"
-}
-];
-
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var currentQuestion = 0;
-var correct;
-var question;
-var sum = correctAnswers + incorrectAnswers;
-var background = $('#background');
-
 $(document).ready(function () {
-    background.hide();
+    var questions = [{
+        question: "In what year did the Tour de France begin?",
+        answerChoices: ["1925", "1903", "1848", "2001"],
+        correctAnswer: "1"
+    },
+    
+    {
+        question: "Who was the first winner of the Tour?",
+        answerChoices: ["Henri Cornet", "Lance Armstrong", "Michael Phelps", "Maurice Garin"],
+        correctAnswer: "3"
+    },
+    
+    {
+        question: "Through which mountain range does the race pass?",
+        answerChoices: ["Alps", "Himalayas", "Pyrenees", "Rockies"],
+        correctAnswer: "0"
+    },
+    
+    {
+        question: "The cyclists must race how many miles per day?",
+        answerChoices: ["500", "400", "100", "200"],
+        correctAnswer: "2"
+    },
+    
+    {
+        question: "What is the total distance the racers will have traveled by the end of the Tour?",
+        answerChoices: ["2,200", "9,050", "800", "1,370"],
+        correctAnswer: "0"
+    }
+    ];
+    
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var currentQuestion = 0;
+    var correct;
+    var question;
+    var background = $('#background');
+    var startOver = $('#startOver');
     var start = $('#start');
+    var clock = $('#clock');
+
+    startGame();
+    background.hide();
     var time = 30;
     var initialOffset = '440';
     var i = 1;
 
     //Starting the game and the clock on click
-    start.click(function () {
-        background.show();
-        $('.circle_animation').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
-        displayQuestion();
-        start.hide();
-        
-        var interval = setInterval(function() {
-                $('#timeRemaining').text(i);
-                if (i === time) {  	
-                    clearInterval(interval);
-                    showResults();
-                    clock.hide();
-            }
-            else {
-                $('.circle_animation').css('stroke-dashoffset', initialOffset-((i+1)*(initialOffset/time)));
-                i++;  
-            }
-        }, 1000);
-    });
+    function startGame () {
+        $('#start').on('click', function() {
+            background.show();
+            start.hide();
+            startOver.hide();
+            $('.circle_animation').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
+            displayQuestion();
+            
+            var interval = setInterval(function() {
+                    $('#timeRemaining').text(i);
+                    if (i === time) {  	
+                        clearInterval(interval);
+                        showResults();
+                        clock.hide();
+                }
+                else {
+                    $('.circle_animation').css('stroke-dashoffset', initialOffset-((i+1)*(initialOffset/time)));
+                    i++;  
+                }
+            }, 1000);
+        })
+        // start.click(function () {
+    // });
+}
+
 
     //Display questions in the DOM
     function displayQuestion() {
@@ -97,15 +105,16 @@ $(document).ready(function () {
         })
     }
     // displayQuestion();
-    
+
     //Function to show Results
     function showResults() {
         if (currentQuestion === 5 || i === 30 ) {
-            $('#correct').html("Correct:" + correctAnswers);
-            $('#incorrect').html("Incorrect:" + incorrectAnswers);
+            $('#correct').html("Correct: " + correctAnswers);
+            $('#incorrect').html("Incorrect: " + incorrectAnswers);
             $('#question').empty();
             $('#form').empty();
             $('#clock').empty();
+            startOver.show();
         }
         else {
             displayQuestion();
